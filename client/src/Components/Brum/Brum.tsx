17fbe5a1c2d26aedc228efc6d8025f67d082b449
toilanome <namntph26878@fpt.edu.vn>
2023-12-15 16:05:20 +0700

    search

--- client/src/Components/Brum/Brum.tsx
+++ client/src/Components/Brum/Brum.tsx
@@ -1,26 +1,47 @@
-import { Link } from "react-router-dom"
-
-const Brum = () => {
-  return (
-    <>
-        <div className="mx-auto max-w-screen-xl">
-  <nav className="text-sm sm:text-base bg-white p-4 md:p-6 lg:p-6 rounded-md shadow-lg">
-    <ol className="list-none p-0 inline-flex space-x-2">
-      <li className="flex items-center">
-        
-      </li>
-      <li className="flex items-center">
-        <Link to={'/'} className="text-gray-600 hover:text-blue-500 transition-colors duration-300">Home</Link>
-        <span className="mx-2">/</span>
-      </li>
-      <li className="flex items-center">
-        <span className="text-gray-800">Products</span>
-      </li>
-    </ol>
-  </nav>
-</div>
-    </>
-  )
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { CiSearch } from "react-icons/ci";
+interface BrumProps {
+  handleSearch: (query: string) => void;
 }
 
-export default Brum
+const Brum: React.FC<BrumProps> = ({ handleSearch }) => {
+  return (
+    <>
+      <div className="mx-auto max-w-screen-xl">
+        <nav className="text-sm sm:text-base bg-white p-4 md:p-6 lg:p-6 rounded-md shadow-lg flex items-center justify-between">
+          <ol className="list-none p-0 inline-flex space-x-2">
+            <li className="flex items-center"></li>
+            <li className="flex items-center">
+              <Link to={'/'} className="text-gray-600 hover:text-blue-500 transition-colors duration-300">
+                Home
+              </Link>
+              <span className="mx-2">/</span>
+            </li>
+            <li className="flex items-center">
+              <span className="text-gray-800">Products</span>
+            </li>
+          </ol>
+          <div className='flex '>
+            <input
+              type="text"
+              placeholder="Tìm kiếm"
+              onChange={(e) => handleSearch(e.target.value)}
+              className='w-full px-4 py-1 text-gray-800  focus:outline-none rounded-l-xl border-1 border-solid border-gray-300 ' style={{border:'1px solid gray'}}
+            />
+            <div>
+            <button className=' flex items-center bg-blue-500 justify-center w-12 h-12 text-white rounded-r-lg text-xl'>
+            <CiSearch />
+            </button>
+            </div>
+            
+          </div>
+
+          
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Brum;
--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,4 +1,4 @@
-import  { useContext } from "react";
+import  { useContext,useState } from "react";
 import Brum from "../Components/Brum/Brum";
 import { ProductShopContext } from "../Context/ProductContext";
 import { Link } from "react-router-dom";
@@ -7,6 +7,12 @@ const ProductPage = () => {
   const { products, isError, isLoading } =
     useContext(ProductShopContext);
   console.log("product", products);
+  const [fillterProduct , setFillterProduct] = useState([])
+
+  const handleSearch = (query: any) =>{
+    const fillter = products?.message?.filter((product:any) => product.title.toLowerCase().includes(query.toLowerCase()))
+    setFillterProduct(fillter)
+  }
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -18,10 +24,54 @@ const ProductPage = () => {
   return (
     <>
       <section className="container ">
-        <Brum />
+        <Brum handleSearch={handleSearch} />
         <h2 className="text-center mt-10 block  pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700  mb-2 font-sans font-bold text-2xl">All Products</h2>
         <div className="grid grid-cols-3">
-        {products?.message?.map((item: any, index: number) => (
+
+      {(fillterProduct.length > 0 ? fillterProduct: products?.message || []).map((item:any, index :number) =>(
+        <div key={index} className="mx-auto max-w-screen-xl mt-10">
+           
+        <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+          <div className="relative mx-4 mt-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+            <img
+              src={item.images}
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div className="p-6">
+            <div className="mb-2 flex items-center justify-between">
+              <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+                {item.title}
+              </p>
+              <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+                $ {item.price}
+              </p>
+            </div>
+            <p className="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+              {item.description}
+            </p>
+          </div>
+          <div className="p-6 pt-0">
+            <Link to={`/products/detail/${item._id}`}>
+            <button
+              className="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none bg-gray-200 text-black-600"
+              type="button"
+            >
+              Add to Cart
+            </button>
+            </Link>
+           
+          </div>
+        </div>
+    </div>
+      )) 
+      
+      }
+
+      
+
+
+        {/* {products?.message?.map((item: any, index: number) => (
 
           <div key={index} className="mx-auto max-w-screen-xl mt-10">
            
@@ -58,7 +108,7 @@ const ProductPage = () => {
                 </div>
               </div>
           </div>
-            ))}
+            ))} */}
 
         </div>
 
